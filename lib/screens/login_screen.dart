17d052aac717e7f0beb7c30e7e8e592af9290643
safi50 <mynameissafiullah786@gmail.com>
2022-04-10 18:18:33 +0500

    login screen updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,6 +8,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// This is the login screen. Prompts user to enter email and password. Then authenticates from Firebase and returns the Chat Screen
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
